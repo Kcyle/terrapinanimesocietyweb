33cb3ddec0acf9b125d39248b36f4702117096ac
Kcyle <134351158+Kcyle@users.noreply.github.com>
2026-02-02 02:38:06 -0500

    Revert to static build for hosting compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://terrapinanimesociety.org',
   base: '/',
-  output: 'server',
-  adapter: vercel(),
   vite: {
     build: {
       cssMinify: true,
